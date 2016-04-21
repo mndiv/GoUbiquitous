7eb90004080056ef59b184d4d0c4bff4b6861938
mndiv <nagadivya.m@gmail.com>
2016-04-21 13:53:49 -0400

    Package name is set same for both mobile and wear applications

--- wear/src/main/java/com/example/android/sunshine/app/WeatherWatchFaceService.java
+++ wear/src/main/java/com/example/android/sunshine/app/WeatherWatchFaceService.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.example.wear.wear;
+package com.example.android.sunshine.app;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -34,11 +34,11 @@ import android.support.wearable.watchface.CanvasWatchFaceService;
 import android.support.wearable.watchface.WatchFaceService;
 import android.support.wearable.watchface.WatchFaceStyle;
 import android.text.format.DateFormat;
-import android.text.format.Time;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.WindowInsets;
 
+import com.example.wear.wear.R;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.wearable.DataApi;
